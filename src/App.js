0b27be3e2b64fc2c99e29c4ada35ce571981da1b
nadezhda <kozlovskayani@yandex.ru>
2022-01-14 15:00:46 +0300

    create clicker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, {Component} from "react";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+    count:0
+    }
+  }
+  increment=()=>{
+    this.setState({count:this.state.count+1})
+  }
+  decrement=()=>{
+    this.setState({count:this.state.count-1})
+  }
+    
+  render() {
+    return (
+      <div className="App"> 
+        
+        <h1>Clicker</h1>
+
+        <h1>
+          {this.state.count}
+        </h1>
+        <button 
+          style={{marginRight:'1rem'}}
+          // type="button" 
+          className="btn btn-success"
+          onClick={this.increment}>
+            Increment
+        </button>
+        <button 
+          // type="button" 
+          className="btn btn-danger"
+          onClick={this.decrement}>
+            Decrement
+        </button>   
+      </div>
+    );
+   }
+  }
+
+
+
 
 export default App;
